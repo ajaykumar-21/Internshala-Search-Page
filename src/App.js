8d3e2c8c8c34140209e96cf3aaf1797e2f68ab43
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-05-31 23:35:53 +0530

    create search bar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import SearchBar from "./components/SearchBar";
+import "./App.css";
 
 function App() {
+  const [internships, setInternships] = useState([]);
+
+  useEffect(() => {
+    fetch("https://internshala.com/hiring/search")
+      .then((res) => res.json())
+      .then((data) => {
+        const internshipsArray = data.internship_ids.map(
+          (id) => data.internships_meta[id]
+        );
+        setInternships(internshipsArray);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  console.log(internships);
   return (
-    <div className="App">
-      
+    <div className="p-4">
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+function SearchBar() {
+  return (
+    <div className="w-full mb-4">
+      <input
+        type="text"
+        placeholder="Search internships..."
+        className="w-full p-2 border rounded"
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
